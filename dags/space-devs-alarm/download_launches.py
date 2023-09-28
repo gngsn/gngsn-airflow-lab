a201f40612a98c94b65709e44b21f2895ff3c283
gngsn <enfj.dev@gmail.com>
2023-09-28 23:41:52 +0900

    modify: change a way of getting source from file to http request

--- dags/space-devs-alarm/download_launches.py
+++ dags/space-devs-alarm/download_launches.py
@@ -9,37 +9,40 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 dag = DAG(
-    dag_id="download_rocket_launches",              # Airflow UI 에 보여질 DAG 이름
-    start_date=airflow.utils.dates.days_ago(14),    # Workflow 가 처음 실행될 일시
+    dag_id="download_rocket_launches",  # Airflow UI 에 보여질 DAG 이름
+    start_date=airflow.utils.dates.days_ago(14),  # Workflow 가 처음 실행될 일시
     schedule_interval=None,
 )
 
 download_launches = BashOperator(
-    task_id = "download_launches",                  # Task 이름
-    bash_command = "curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",
-    dag = dag,                                      # DAG 변수
+    task_id="download_launches",  # Task 이름
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",
+    dag=dag,  # DAG 변수
 )
 
 
 def _get_pictures():
     pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
 
-    # Download all pictures in launches.json
-    with open("/tmp/launches.json") as f:
-        launches = json.load(f)
-        image_urls = [launch["image"] for launch in launches["results"]]
-        for image_url in image_urls:
-            try:
-                response = requests.get(image_url)
-                image_filename = image_url.split("/")[-1]
-                target_file = f"/tmp/images/{image_filename}"
-                with open(target_file, "wb") as f:
-                    f.write(response.content)
-                print(f"Downloaded {image_url} to {target_file}")
-            except requests_exceptions.MissingSchema:
-                print(f"{image_url} appears to be an invalid URL.")
-            except requests_exceptions.ConnectionError:
-                print(f"Could not connect to {image_url}.")
+    response = requests.get('https://httpbin.org/basic-auth/user/pass')
+    print("response: ", response)
+    launches = response.json()
+
+    print("launches: ", launches)
+
+    image_urls = [launch["image"] for launch in launches["results"]]
+    for image_url in image_urls:
+        try:
+            response = requests.get(image_url)
+            image_filename = image_url.split("/")[-1]
+            target_file = f"/tmp/images/{image_filename}"
+            with open(target_file, "wb") as f:
+                f.write(response.content)
+            print(f"Downloaded {image_url} to {target_file}")
+        except requests_exceptions.MissingSchema:
+            print(f"{image_url} appears to be an invalid URL.")
+        except requests_exceptions.ConnectionError:
+            print(f"Could not connect to {image_url}.")
 
 
 get_pictures = PythonOperator(
