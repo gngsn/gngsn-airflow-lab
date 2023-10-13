70b0097757512e481cde24b7de28e92b9042a66b
gngsn <enfj.dev@gmail.com>
2023-10-13 23:47:26 +0900

    docs: add README file and some comment

--- dags/increamental-data/wikimedia.py
+++ dags/increamental-data/wikimedia.py
@@ -13,13 +13,13 @@ get_data = BashOperator(
     bash_command=(
         "curl -o /tmp/wikipageviews.gz "
         "https://dumps.wikimedia.org/other/pageviews/"
-        "{{ execution_date.year }}/"
+        "{{ execution_date.year }}/"        # {{Double curly braces}} 표시는 런타임 시 입력될 변수를 나타냄
         "{{ execution_date.year }}-"
         "{{ '{:02}'.format(execution_date.month) }}/"
         "pageviews-{{ execution_date.year }}"
         "{{ '{:02}'.format(execution_date.month) }}"
         "{{ '{:02}'.format(execution_date.day) }}-"
-        "{{ '{:02}'.format(execution_date.hour) }}0000.gz"
+        "{{ '{:02}'.format(execution_date.hour) }}0000.gz" # 파이썬 변수나 표현식이 제공될 수 있음
     ),
     dag=dag,
 )
