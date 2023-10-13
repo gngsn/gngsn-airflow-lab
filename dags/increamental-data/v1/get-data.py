89bb8d8413b977cfd29be3848fbbb4e55d4b9696
gngsn <enfj.dev@gmail.com>
2023-10-13 23:56:27 +0900

    add: example v2

--- dags/increamental-data/v1/get-data.py
+++ dags/increamental-data/v1/get-data.py
@@ -23,3 +23,4 @@ get_data = BashOperator(
     ),
     dag=dag,
 )
+
---
+++ dags/increamental-data/v2/get-data.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+import airflow.utils.dates
+from airflow.operators.python import PythonOperator
+
+from urllib import request
+
+dag = DAG(
+    dag_id="stocksense_bashoperator",
+    start_date=airflow.utils.dates.days_ago(3),
+    schedule_interval="@hourly",
+)
+
+def _get_data(year, month, day, hour, output_path, **_):
+    url = (
+        "https://dumps.wikimedia.org/other/pageviews/"
+        f"{year}/{year}-{month:0>2}/"
+        f"pageviews-{year}{month:0>2}{day:0>2}-{hour:0>2}0000.gz"
+    )
+    request.urlretrieve(url, output_path)
+
+
+get_data = PythonOperator(
+    task_id="get_data",
+    python_callable=_get_data,
+    op_kwargs={
+        "year": "{{ execution_date.year }}",
+        "month": "{{ execution_date.month }}",
+        "day": "{{ execution_date.day }}",
+        "hour": "{{ execution_date.hour }}",
+        "output_path": "/tmp/wikipageviews.gz",
+    },
+    dag = dag,
+)
