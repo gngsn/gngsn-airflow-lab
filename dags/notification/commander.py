6c7be34fe0fc20769be93d5f3f08aa8dbb8b53e4
gngsn <enfj.dev@gmail.com>
2024-01-26 22:35:20 +0900

    fix: Commander 오류 제거: 구현 TODO 처리

--- dags/notification/commander.py
+++ dags/notification/commander.py
@@ -1,4 +1,3 @@
-import operator
 from abc import ABC, abstractmethod
 
 from notification.persistence.base import DB
@@ -10,25 +9,26 @@ class Command(ABC):
         pass
 
 
-class Predicator(Command):
-
-    def __init__(self, value):
-        self.value = value
-
-    stack = []
-    comparison_ops = ['==', '<=', '>=', '<', '>']
-    logical_ops = ['and', 'or']
-    def is_command(self, value: str):
-        return (self.comparison_ops + self.logical_ops).count(value) < 1
-
-    def execute(self):
-        split: list[str] = self.value.split(' ')
-
-        for s in split:
-            if self.is_command(s):
-                update(pre_op, num)
-            elif s in '+-*/':
-                # num = 10 * num + int(s)
+# TODO
+# class Predicator(Command):
+#
+#     def __init__(self, value):
+#         self.value = value
+#
+#     stack = []
+#     comparison_ops = ['==', '<=', '>=', '<', '>']
+#     logical_ops = ['and', 'or']
+#     def is_command(self, value: str):
+#         return (self.comparison_ops + self.logical_ops).count(value) < 1
+#
+#     def execute(self):
+#         split: list[str] = self.value.split(' ')
+#
+#         for s in split:
+#             if self.is_command(s):
+#
+#             elif s in '+-*/':
+# num = 10 * num + int(s)
 
 
 class SQLCommand(Command):
